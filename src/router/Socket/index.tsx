7104ce7977e17e72b5a995b90fb3fd2eeec8bd81
dnt201.usum <nha.tran@usumsoftware.com>
2022-12-17 01:34:05 +0700

    Fix Socket

--- src/router/Socket/index.tsx
+++ src/router/Socket/index.tsx
@@ -25,99 +25,91 @@ export const Post_Vote = () => {
   socket.on("Post_Vote", (msg) => {
     console.log("Websocket Post Vote received!");
     console.log(msg);
-    if (msg.userId) {
-      userApi.getUserDetail(msg.userId).then((result) => {
-        console.log(result);
-        if (result.status === 200) {
-          toast.custom(
-            (t) => (
-              <div
-                className={`${
-                  t.visible ? "animate-enter" : "animate-leave"
-                } max-w-md w-full text-primary  bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-              >
-                <div className="flex-1 w-0 p-4">
-                  <div className="flex items-start">
-                    <div className="flex-shrink-0 pt-0.5">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={result.data.avatarLink || userDefault}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3 flex-1">
-                      <p className="text-sm font-medium text-gray-900">
-                        {result.data.username}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">{msg.body}</p>
-                    </div>
-                  </div>
-                  7``
+    if (msg) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`${
+              t.visible ? "animate-enter" : "animate-leave"
+            } max-w-md w-full text-primary   bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start relative">
+                <p className=" text-bg font-medium text-right text-[8px] absolute top-0 -translate-y-2 translate-x-1 right-0">
+                  Now
+                </p>
+                <div className="flex-shrink-0 pt-0.5">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={msg.userSend.imageLink || userDefault}
+                    alt=""
+                  />
                 </div>
-                <div className="flex border-l border-gray-200">
-                  <button
-                    onClick={() => toast.dismiss(t.id)}
-                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  >
-                    Close
-                  </button>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {msg.userSend.username}
+                  </p>
+                  <p className="mt-1 text-sm  text-bg">{msg.body}</p>
                 </div>
               </div>
-            ),
-            { duration: 4000 }
-          );
-        }
-      });
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        { duration: 4000 }
+      );
     }
-
-    // return cb(null, msg);
   });
   socket.on("Post_Comment", (msg) => {
     console.log("Websocket Post Comment received!");
     console.log(msg);
-    if (msg.userId) {
-      userApi.getUserDetail(msg.userId).then((result) => {
-        console.log(result);
-        if (result.status === 200) {
-          toast.custom(
-            (t) => (
-              <div
-                className={`${
-                  t.visible ? "animate-enter" : "animate-leave"
-                } max-w-md w-full text-primary  bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-              >
-                <div className="flex-1 w-0 p-4">
-                  <div className="flex items-start">
-                    <div className="flex-shrink-0 pt-0.5">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={result.data.avatarLink || userDefault}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3 flex-1">
-                      <p className="text-sm font-medium text-gray-900">
-                        {result.data.username}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">{msg.body}</p>
-                    </div>
-                  </div>
-                  7``
+    if (msg) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`${
+              t.visible ? "animate-enter" : "animate-leave"
+            } max-w-md w-full text-primary   bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start relative">
+                <p className=" text-bg font-medium text-right text-[8px] absolute top-0 -translate-y-2 translate-x-1 right-0">
+                  Now
+                </p>
+                <div className="flex-shrink-0 pt-0.5">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={msg.userSend.imageLink || userDefault}
+                    alt=""
+                  />
                 </div>
-                <div className="flex border-l border-gray-200">
-                  <button
-                    onClick={() => toast.dismiss(t.id)}
-                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  >
-                    Close
-                  </button>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {msg.userSend.username}
+                  </p>
+                  <p className="mt-1 text-sm  text-bg">{msg.body}</p>
                 </div>
               </div>
-            ),
-            { duration: 4000 }
-          );
-        }
-      });
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        { duration: 4000 }
+      );
     }
 
     // return cb(null, msg);
@@ -125,49 +117,46 @@ export const Post_Vote = () => {
   socket.on("Post_Comment_Vote", (msg) => {
     console.log("Websocket Post Comment received!");
     console.log(msg);
-    if (msg.userId) {
-      userApi.getUserDetail(msg.userId).then((result) => {
-        console.log(result);
-        if (result.status === 200) {
-          toast.custom(
-            (t) => (
-              <div
-                className={`${
-                  t.visible ? "animate-enter" : "animate-leave"
-                } max-w-md w-full text-primary  bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-              >
-                <div className="flex-1 w-0 p-4">
-                  <div className="flex items-start">
-                    <div className="flex-shrink-0 pt-0.5">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={result.data.avatarLink || userDefault}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3 flex-1">
-                      <p className="text-sm font-medium text-gray-900">
-                        {result.data.username}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">{msg.body}</p>
-                    </div>
-                  </div>
-                  7``
+    if (msg) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`${
+              t.visible ? "animate-enter" : "animate-leave"
+            } max-w-md w-full text-primary   bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start relative">
+                <p className=" text-bg font-medium text-right text-[8px] absolute top-0 -translate-y-2 translate-x-1 right-0">
+                  Now
+                </p>
+                <div className="flex-shrink-0 pt-0.5">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={msg.userSend.imageLink || userDefault}
+                    alt=""
+                  />
                 </div>
-                <div className="flex border-l border-gray-200">
-                  <button
-                    onClick={() => toast.dismiss(t.id)}
-                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  >
-                    Close
-                  </button>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {msg.userSend.username}
+                  </p>
+                  <p className="mt-1 text-sm  text-bg">{msg.body}</p>
                 </div>
               </div>
-            ),
-            { duration: 4000 }
-          );
-        }
-      });
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        { duration: 4000 }
+      );
     }
 
     // return cb(null, msg);
@@ -175,49 +164,46 @@ export const Post_Vote = () => {
   socket.on("Post_Reply", (msg) => {
     console.log("Websocket Post Comment received!");
     console.log(msg);
-    if (msg.userId) {
-      userApi.getUserDetail(msg.userId).then((result) => {
-        console.log(result);
-        if (result.status === 200) {
-          toast.custom(
-            (t) => (
-              <div
-                className={`${
-                  t.visible ? "animate-enter" : "animate-leave"
-                } max-w-md w-full text-primary  bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-              >
-                <div className="flex-1 w-0 p-4">
-                  <div className="flex items-start">
-                    <div className="flex-shrink-0 pt-0.5">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={result.data.avatarLink || userDefault}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3 flex-1">
-                      <p className="text-sm font-medium text-gray-900">
-                        {result.data.username}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">{msg.body}</p>
-                    </div>
-                  </div>
-                  7``
+    if (msg) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`${
+              t.visible ? "animate-enter" : "animate-leave"
+            } max-w-md w-full text-primary   bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start relative">
+                <p className=" text-bg font-medium text-right text-[8px] absolute top-0 -translate-y-2 translate-x-1 right-0">
+                  Now
+                </p>
+                <div className="flex-shrink-0 pt-0.5">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={msg.userSend.imageLink || userDefault}
+                    alt=""
+                  />
                 </div>
-                <div className="flex border-l border-gray-200">
-                  <button
-                    onClick={() => toast.dismiss(t.id)}
-                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  >
-                    Close
-                  </button>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {msg.userSend.username}
+                  </p>
+                  <p className="mt-1 text-sm  text-bg">{msg.body}</p>
                 </div>
               </div>
-            ),
-            { duration: 4000 }
-          );
-        }
-      });
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        { duration: 4000 }
+      );
     }
 
     // return cb(null, msg);
